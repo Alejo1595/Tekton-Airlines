b3d4f5f4d2f9e275acaceaecfb226460aa840215
Julian Sanchez <alejo15san95@gmail.com>
2022-05-28 18:13:36 -0500

    Creating form sections dynamically

--- src/app/passenger-registration/passenger-registration.module.ts
+++ src/app/passenger-registration/passenger-registration.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { SharedModule } from '../shared/shared.module';
 
 import { PassengerRegistrationRoutingModule } from './passenger-registration-routing.module';
 
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { PassegerListComponent } from './passeger-list/passeger-list.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 
 @NgModule({
@@ -16,8 +24,15 @@ import { PassegerListComponent } from './passeger-list/passeger-list.component';
   ],
   imports: [
     CommonModule,
-    PassengerRegistrationRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     SharedModule,
+    PassengerRegistrationRoutingModule,
   ],
   exports: [
     RegistrationFormComponent,
--- src/app/passenger-registration/registration-form/registration-form.component.ts
+++ src/app/passenger-registration/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup = this.formBuilder.group({});
 
-  ngOnInit(): void {
+  public get secciones() {
+    return this.formulario.get('secciones') as FormArray;
   }
 
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = formBuilder.group({
+      secciones: formBuilder.array([])
+    })
+  }
+
+  ngOnInit(): void {
+    this.agregarSeccion();
+  }
+
+  public submit = () => {
+    console.log(this.formulario.controls);
+  }
+
+  public agregarSeccion = (): void =>{
+    const seccion = this.formBuilder.group({
+      nombres: [null, [Validators.required]],
+      apellidos: [null, Validators.required],
+      nacionalidad: [null, Validators.required],
+      tipoDocumento: [null, Validators.required],
+      numeroDocumento: [null, Validators.required]
+    });
+
+    this.secciones.push(seccion);
+
+  }
 }
